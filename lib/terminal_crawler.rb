4055c4114100bc34310fca20bd170d00c2ed3308
Aidan McGuire <>
2021-06-25 17:14:05 -0600

    Add rspec

--- lib/terminal_crawler.rb
+++ lib/terminal_crawler.rb
@@ -10,10 +10,17 @@ class TerminalCrawler
     @exceptions = [Mechanize::ResponseCodeError, Net::HTTP::Persistent::Error]
   end
   
+  def get_profiles_with_bad_links
+    broken = get_profile_links.first(5).map do |link|
+      check_status(link)
+    end
+    broken.reject { |i| i.empty? }
+  end
+  
   def get_profile_links
     profiles = @mechanize.page.links_with(:text => @see_profile_text)
   end
-
+  
   def check_status(profile)
     broken_profiles = []
     profile.click.links_with(:text => @launch_app_text).find_all do |link|
@@ -24,12 +31,5 @@ class TerminalCrawler
       end
     end
     broken_profiles
-  end
-
-  def get_profiles_with_bad_links
-    broken = get_profile_links.first(5).map do |link|
-      check_status(link)
-    end
-    broken.reject { |i| i.empty? }
   end
 end
---
+++ spec/terminal_crawler_spec.rb
@@ -0,0 +1,8 @@
+require './spec/spec-helper'
+require './lib/terminal_crawler'
+
+RSpec.describe TerminalCrawler do
+  it 'can get links to alums terminal profiles' do
+    
+  end
+end

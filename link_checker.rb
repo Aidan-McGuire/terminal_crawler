584403c85bef7ed0eeaf7b8d46826d440ac63b8e
Aidan McGuire <>
2021-06-25 13:48:05 -0600

    Initial commit

---
+++ link_checker.rb
@@ -0,0 +1,22 @@
+# require 'nokogiri'
+# require 'open-uri'
+require 'mechanize'
+
+class TerminalCrawler
+
+  def initialize
+    @url = 'https://terminal.turing.edu'
+    @agent = Mechanize.new
+    @page = @agent.get(@url)
+  end
+  
+  def get_links
+    profiles = @agent.page.links_with(:text => "\n                See full profile\n                \n")
+  end
+
+  def check_status(profile)
+    profile.links_with(:text => "\n          Launch the App\n").find_all do |link|
+      link.click.code.to_i >= 400
+    end
+  end
+end

664ed57df77f451ee10869975697994c26f4d2c1
Aidan McGuire <aidanmcguire211@gmail.com>
2021-08-01 14:58:55 -0600

    Add additional expectation to retrieve_profile_content spec

--- spec/terminal_crawler_spec.rb
+++ spec/terminal_crawler_spec.rb
@@ -1,7 +1,7 @@
 require './spec/spec_helper'
 require './lib/terminal_crawler'
 
-RSpec.describe TerminalCrawler do
+RSpec.describe TerminalCrawler, :vcr do
   context 'retrieve profile links' do
     it 'return an array of links to all alumni profiles' do
       links = TerminalCrawler.retrieve_profile_links
@@ -15,12 +15,14 @@ RSpec.describe TerminalCrawler do
     it 'return an array of links to all alumni profiles' do
       content = TerminalCrawler.retrieve_profile_content
       expect(content).to be_a Hash
-      expect(content.count).to eq(200)
       expect(content["bendelonlee@gmail.com"]).to eq([
         "https://glass-planner-frontend-git-demo-bendelonlee.vercel.app/",
         "https://thirsty-plants.herokuapp.com",
         "http://sweater-weather-1.surge.sh",
         "https://book-club-project.herokuapp.com"
+        ])
+      expect(content["aidanmcguire211@gmail.com"]).to eq([
+        "https://headtotoe.surge.sh/"
         ])
     end
   end

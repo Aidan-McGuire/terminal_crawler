29f8d3c8eb02e085bd1f9c27d5285a7cea247bfe
Aidan McGuire <aidanmcguire211@gmail.com>
2021-08-01 14:07:08 -0600

    Add method retrieve_profile_content

--- lib/terminal_crawler.rb
+++ lib/terminal_crawler.rb
@@ -1,14 +1,31 @@
 require 'open-uri'
 require 'nokogiri'
+class TerminalCrawler
+  class << self
+    def retrieve_profile_links
+      doc = Nokogiri::HTML(URI.open('https://terminal.turing.edu'))
+      elements = doc.css('a:contains("See full profile")')
+      profile_links = elements.map do |ele|
+        ele['href']
+      end
+    end
 
-url = 'https://terminal.turing.edu'
-see_profile_text = "\n                See full profile\n                \n"
-launch_app_text = "\n          Launch the App\n"
-email_text = "\n        Email Directly\n"
+    def retrieve_profile_content
+      profile_content = Hash.new { |hash, key| hash[key] = [] }
+      profile_links = retrieve_profile_links
+      profile_links.each do |link|
+        profile = Nokogiri::HTML(URI.open("https://terminal.turing.edu#{link}"))
+        elements = profile.css('a:contains("Launch the App")')
 
-doc = Nokogiri::HTML(URI.open(url))
-elements = doc.css('a:contains("See full profile")')
-profile_links = elements.map do |ele|
-  ele['href']
+        project_links = elements.map do |link|
+          link['href']
+        end
+
+        alum_email_element = profile.css('a:contains("Email Directly")')
+        email = alum_email_element.first['href'].split(':').last
+        profile_content[email] = project_links
+        sleep(5)
+      end
+    end
+  end
 end
-require 'pry'; binding.pry

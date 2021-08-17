ad7af62afcf44f0dd9af7a274704b11db8ca14b6
Aidan McGuire <aidanmcguire211@gmail.com>
2021-08-17 16:46:20 -0600

    Update tests as terminal changed

--- spec/terminal_crawler_spec.rb
+++ spec/terminal_crawler_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe TerminalCrawler, :vcr do
     it 'returns an array of links to all alumni profiles' do
       links = TerminalCrawler.retrieve_profile_links
       expect(links).to be_an Array
-      expect(links.count).to eq(121)
+      expect(links.count).to eq(123)
       expect(links.first).to eq('/alumni/675-ben-lee')
     end
   end
@@ -37,32 +37,39 @@ RSpec.describe TerminalCrawler, :vcr do
 
   context 'check links' do
     it 'returns broken project links' do
-      links = ["http://chesspedition.herokuapp.com", "http://leahlamarr.com", "http://sweater-weather-1.surge.sh", "https://book-club-project.herokuapp.com", "https://sweater-weather-2102.herokuapp.com/"]
+      links = [
+        "http://chesspedition.herokuapp.com", "http://leahlamarr.com", 
+        "http://sweater-weather-1.surge.sh", "https://book-club-project.herokuapp.com", 
+        "https://sweater-weather-2102.herokuapp.com/", "http://mixdup.vercel.app/",
+        "https://whosthatpokemongame.netlify.app/game", "https://ecosystem.theorem.local:3300/",
+        "https://shrouded-hamlet-60350.herokuapp.com/#/", "https://dream-home-cap.herokuapp.com/",
+        "https://monstronomicon.herokuapp.com/", "https://rancid-tomatillos-lm-kd.herokuapp.com/",
+        "https://penpost-web.vercel.app/", "https://carryokay.netlify.app/songbook",
+        "https://ancient-ridge-85691.herokuapp.com"
+      ]
 
-      expect(TerminalCrawler.check_links(links)).to eq(["https://sweater-weather-2102.herokuapp.com/"])
+      expect(TerminalCrawler.check_links(links)).to eq([
+        "https://whosthatpokemongame.netlify.app/game", "https://ecosystem.theorem.local:3300/",
+        "https://shrouded-hamlet-60350.herokuapp.com/#/", "https://dream-home-cap.herokuapp.com/",
+        "https://monstronomicon.herokuapp.com/", "https://rancid-tomatillos-lm-kd.herokuapp.com/",
+        "https://penpost-web.vercel.app/", "https://carryokay.netlify.app/songbook",
+        "https://ancient-ridge-85691.herokuapp.com"
+      ])
     end
   end
 
   context 'sanitize(links)' do
-    it 'appends https to beginning of url if protocol not present' do
-      tricky_links = ["chesspedition.herokuapp.com", "leahlamarr.com", "http://sweater-weather-1.surge.sh", "https://book-club-project.herokuapp.com", "http://sweater-weather-1.surge.sh", "https://book-club-project.herokuapp.com"]
-      poorly_formatted_links = [" https://astro-clash.surge.sh/", "leahlamarr.com"]
+    it 'appends http to beginning of url if protocol not present' do
+      tricky_links = ["chesspedition.herokuapp.com", "leahlamarr.com", "http://sweater-weather-1.surge.sh", "https://book-club-project.herokuapp.com"]
 
 
-      expect(TerminalCrawler.sanitize(tricky_links)).to eq(["https://chesspedition.herokuapp.com", "https://leahlamarr.com", "http://sweater-weather-1.surge.sh", "https://book-club-project.herokuapp.com", "http://sweater-weather-1.surge.sh", "https://book-club-project.herokuapp.com"])
-      expect(TerminalCrawler.sanitize(poorly_formatted_links)).to eq(["https://astro-clash.surge.sh/", "https://leahlamarr.com"])
-    end
-
-    it 'replaces http with https' do
-      links = ["http://mixdup.vercel.app/", "http://leahlamarr.com"]
-
-      expect(TerminalCrawler.sanitize(links)).to eq(["https://mixdup.vercel.app/", "https://leahlamarr.com"])
+      expect(TerminalCrawler.sanitize(tricky_links)).to eq(["http://chesspedition.herokuapp.com", "http://leahlamarr.com", "http://sweater-weather-1.surge.sh", "https://book-club-project.herokuapp.com"])
     end
 
     it 'removes extra whitespace' do
       poorly_formatted_links = [" https://astro-clash.surge.sh/", "leahlamarr.com "]
 
-      expect(TerminalCrawler.sanitize(poorly_formatted_links)).to eq(["https://astro-clash.surge.sh/", "https://leahlamarr.com"])
+      expect(TerminalCrawler.sanitize(poorly_formatted_links)).to eq(["https://astro-clash.surge.sh/", "http://leahlamarr.com"])
     end
   end
 end

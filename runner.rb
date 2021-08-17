303cd7c7985fd92ff78166b046f9c6ba121f6722
Aidan McGuire <aidanmcguire211@gmail.com>
2021-08-17 15:18:23 -0600

    Refactor to accomdate args refactor

--- runner.rb
+++ runner.rb
@@ -1,6 +1,8 @@
 require './lib/terminal_crawler.rb'
 require 'pry'
 
-# profiles = TerminalCrawler.retrieve_broken_profiles
-links = TerminalCrawler.check_links(["http://mixdup.vercel.app/"])
-binding.pry
+
+profile_links = TerminalCrawler.retrieve_profile_links
+project_links = TerminalCrawler.retrieve_profile_content(profile_links)
+broken_profiles = TerminalCrawler.retrieve_broken_profiles(project_links)
+

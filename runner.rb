aabaaa92ea69aa99bee0b3d1c61e451fc69c6bb5
Aidan McGuire <aidanmcguire211@gmail.com>
2021-08-18 16:05:55 -0600

    Add comments for clarity

--- runner.rb
+++ runner.rb
@@ -1,6 +1,6 @@
 require './lib/terminal_crawler.rb'
 require 'pry'
 
-profile_links = TerminalCrawler.retrieve_profile_links
-project_links = TerminalCrawler.retrieve_profile_content(profile_links)
-broken_profiles = TerminalCrawler.retrieve_broken_profiles(project_links)
+profile_links = TerminalCrawler.retrieve_profile_links # collect links to all profiles
+project_links = TerminalCrawler.retrieve_profile_content(profile_links) # collect project links from each profile
+broken_profiles = TerminalCrawler.retrieve_broken_profiles(project_links) # find all broken project links

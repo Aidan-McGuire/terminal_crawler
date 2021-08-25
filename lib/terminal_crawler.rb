97e56863fd29eb7fcb98d2aab61143ba7f0dee85
Aidan McGuire <aidanmcguire211@gmail.com>
2021-08-25 17:27:39 -0600

    Remove old comments

--- lib/terminal_crawler.rb
+++ lib/terminal_crawler.rb
@@ -29,17 +29,5 @@ class TerminalCrawler
       end
       profile_content
     end
-
-    # def retrieve_emails(broken_links, profile_content)
-    #   broken_profiles = Hash.new { |hash, key| hash[key] = [] }
-    #   broken_links.each do |broken_link|
-    #     profile_content.each do |email, links|
-    #       if !links.grep(broken_link).empty?
-    #         broken_profiles[email] << broken_link
-    #       end
-    #     end
-    #   end
-    #   broken_profiles
-    # end
   end
 end
